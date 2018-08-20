b3a9e74ac20a4f6546b0a5ea9d89f2fe85410f02
江伟维 <jiangweiwei@qccr.com>
2018-08-20 14:16:49 +0800

    modified:异步获取level的值

--- treerecyclerview/src/main/java/wellijohn/org/treerecyclerview/adapter/BaseTreeRVAdapter.java
+++ treerecyclerview/src/main/java/wellijohn/org/treerecyclerview/adapter/BaseTreeRVAdapter.java
@@ -4,6 +4,13 @@ import android.support.v7.widget.RecyclerView;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import wellijohn.org.treerecyclerview.ex.StopMsgException;
 import wellijohn.org.treerecyclerview.vo.Tree;
 
@@ -16,14 +23,42 @@ import wellijohn.org.treerecyclerview.vo.Tree;
 public abstract class BaseTreeRVAdapter<M extends Tree> extends BaseRVAdapterV2<RecyclerView.ViewHolder, M> {
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
-        try {
-            getLevel(position, mDatas, new PosBean());
-        } catch (StopMsgException ex) {
-            int type = Integer.parseInt(ex.getMessage());
-            M tree = ex.getTree();
-            onBindViewHolder(type, tree, holder);
-        }
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+        Observable.create(new ObservableOnSubscribe<StopMsgException>() {
+            @Override
+            public void subscribe(ObservableEmitter<StopMsgException> emitter) throws StopMsgException {
+                try {
+                    getLevel(position, mDatas, new PosBean());
+                } catch (StopMsgException ex) {
+                    emitter.onNext(ex);
+                }
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<StopMsgException>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(StopMsgException ex) {
+                        int type = Integer.parseInt(ex.getMessage());
+                        M tree = ex.getTree();
+                        onBindViewHolder(type, tree, holder);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 
 
